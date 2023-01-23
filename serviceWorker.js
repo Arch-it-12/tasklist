0b3c330ae5b569dce6ad60417c89fac55f06cda4
Nikhil Kakani <nikhilkakani910@gmail.com>
2023-01-23 14:10:07 -0500

    implemented pwa (hopefully)

---
+++ serviceWorker.js
@@ -0,0 +1,29 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+    "/",
+    "/tasklist/ui/templates/",
+    "/tasklist/ui/static/style.css",
+    "/tasklist/ui/static/js/script.js",
+    "/tasklist/ui/static/icecream.webp",
+    "/tasklist/ui/templates/admin_panel.html",
+    "/tasklist/ui/templates/base.html",
+    "/tasklist/ui/templates/home.html",
+    "/tasklist/ui/templates/tasks.html",
+    "/tasklist/ui/templates/user_list.html"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCoffee).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
